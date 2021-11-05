c75ddeb212334ef9535d5245c1d45adb66ead9f6
dantekansepa <88306354+dantekansepa@users.noreply.github.com>
2021-11-05 14:43:27 +0200

    Removing whitespace

--- get_path.c
+++ get_path.c
@@ -9,40 +9,38 @@
 
 char *find_path(char *filename, char *tmp, char *er)
 {
-	      DIR *dir;
-	      struct dirent *sd;
-	      char *file_path, *path, *ret;
-	      int len = 0;
+DIR *dir;
+struct dirent *sd;
+char *file_path, *path, *ret;
+int len = 0;
 
-	      while (filename[len]){
-                len++;
-        }    
-	      path = _getenv("PATH");
-	      tmp = save_path(tmp, path);
-	      file_path = strtok(tmp, ":");
-	      while (file_path)
-	      {
-		            dir = opendir(file_path);
-		            if (!dir) {
-			                  printf("Error! Unable to open directory.\n");
-			                  exit(0);
-                }
-		    }
-		            while ((sd = readdir(dir)))
-		           {
-			                  ret = read_dir(er, sd, filename, len, file_path, tmp);
-			                  if (ret != er){
-				                        closedir(dir);
-				                        if (!(access(ret, X_OK)))
-					                              return (ret);
-			                  }
-		            }
-		            closedir(dir);
-		            file_path = strtok(NULL, ":");
-	      }
-	      path = NULL;
-	      free(tmp);
-	      return (er);
+while (filename[len]){
+len++;
+}
+path = _getenv("PATH");
+tmp = save_path(tmp, path);
+file_path = strtok(tmp, ":");
+while (file_path){
+dir = opendir(file_path);
+if (!dir){
+printf("Error! Unable to open directory.\n");
+exit(0);
+}
+}
+while ((sd = readdir(dir))){
+ret = read_dir(er, sd, filename, len, file_path, tmp);
+if (ret != er){
+closedir(dir);
+if (!(access(ret, X_OK)))
+return (ret);
+}
+}
+closedir(dir);
+file_path = strtok(NULL, ":");
+}
+path = NULL;
+free(tmp);
+return (er);
 }
 
 /**
@@ -57,22 +55,21 @@ char *find_path(char *filename, char *tmp, char *er)
  */
 char *read_dir(char *er, struct dirent *s, char *fil, int l, char *fp, char *t)
 {
-	       int i = 0;
-	       char *ret;
+int i = 0;
+char *ret;
 
-	       for (i = 0; s->d_name[i] && fil[i]; i++)
-	       {
-		              if (s->d_name[i] != fil[i])
-			            break;
-		              if (i == (l - 1) && !(s->d_name[i + 1]))
-		              {
-			              ret = strcat(fp, "/");
-			              ret = strcat(ret, fil);
-			              free(t);
-			              return (ret);
-		              }
-	      }
-	      return (er);
+for (i = 0; s->d_name[i] && fil[i]; i++){
+if (s->d_name[i] != fil[i])
+break;
+if (i == (l - 1) && !(s->d_name[i + 1]))
+{
+ret = strcat(fp, "/");
+ret = strcat(ret, fil);
+free(t);
+return (ret);
+}
+}
+return (er);
 }
 /**
  *save_path - saves a copy of the PATH string
@@ -83,30 +80,28 @@ char *read_dir(char *er, struct dirent *s, char *fil, int l, char *fp, char *t)
  */
 char *save_path(char *tmp, char *path)
 {
-	      int i = 0;
+int i = 0;
 
-	      if (!tmp)
-	      {
-		            tmp = malloc(sizeof(char) * 100);
-		            while (path[i])
-		            {
-			                  tmp[i] = path[i];
-			                  i++;
-			                  tmp[i] = '\0';
-		            }
-		            i = 0;
-		            return (tmp);
-	      }
-	      else
-	      {
-		            while (tmp[i])
-		            {
-			                  path[i] = tmp[i];
-			                  i++;
-			                  path[i] = '\0';
-		            }
-		            i = 0;
-		            return (path);
-	      }
-	      return ("error");
+if (!tmp){
+tmp = malloc(sizeof(char) * 100);
+while (path[i]){
+tmp[i] = path[i];
+i++;
+tmp[i] = '\0';
+}
+i = 0;
+return (tmp);
+}
+else
+{
+while (tmp[i])
+{
+path[i] = tmp[i];
+i++;
+path[i] = '\0';
+}
+i = 0;
+return (path);
+}
+return ("error");
 }
